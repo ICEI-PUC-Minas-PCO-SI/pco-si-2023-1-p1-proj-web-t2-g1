4c5a57c6fe24c752d628b7ce3d004fe098d7964b
ArthurParreiras <126624297+ArthurParreiras@users.noreply.github.com>
2023-06-24 01:12:50 -0300

    Otimização Avaliação

--- avaliar.js
+++ avaliar.js
@@ -1,50 +1,67 @@
-URL='http://localhost:3000/comments';
+URL = 'http://localhost:3000/comments';
 
-// Função para calcular a média
-function calcularMedia(avaliacoes) {
-    const notas = avaliacoes.map(avaliacao => parseInt(avaliacao.nota, 10));
-    const soma = notas.reduce((acc, nota) => acc + nota, 0);
-    const media = soma / notas.length;
-  
-    return media.toFixed(2);
-  }
-  
-  // Função para manipular o evento de envio do formulário
-  function handleFormSubmit(event) {
+function buscarNomeUsuario(usuarios, userId) {
+    const usuario = usuarios.find(user => user.id === userId);
+    return usuario ? usuario.username : '';
+}
+
+// média
+function handleFormSubmit(event) {
     event.preventDefault();
-  
+
     const selectNota = document.getElementById('selectNota');
     const nota = selectNota.value;
-  
-    const nomeUsuario = 'João Pedro da Silva Santos'; // Nome do usuário fixo para este exemplo
-  
-    const avaliacao = { nomeUsuario, nota };
-  
-    // Ler o arquivo JSON existente (se houver)
-    let avaliacoesExistente = [];
-    try {
-      avaliacoesExistente = JSON.parse(localStorage.getItem('avaliacoes')) || [];
-    } catch (error) {
-      console.error('Erro ao ler o arquivo JSON:', error);
-    }
-  
-    // Adicionar a nova avaliação às avaliações existentes
-    avaliacoesExistente.push(avaliacao);
-  
-    // Calcular a média das avaliações
-    const media = calcularMedia(avaliacoesExistente);
-  
-    // Salvar a média no arquivo JSON
-    const jsonContent = JSON.stringify({ nomeUsuario, media });
-    try {
-      localStorage.setItem('avaliacoes', jsonContent);
-      console.log('Média de avaliações armazenada com sucesso!');
-    } catch (error) {
-      console.error('Erro ao gravar o arquivo JSON:', error);
-    }
-  }
-  
-  // Associar o evento de envio do formulário à função de manipulação
-  const form = document.getElementById('avaliacaoForm');
-  form.addEventListener('submit', handleFormSubmit);
-  
+
+    const selectUsuario = document.getElementById('selectUsuario');
+    const userId = selectUsuario.value;
+
+    // Dados api
+    fetch('db_usuarios.json')
+        .then(response => response.json())
+        .then(data => {
+            // Buscar o nome de usuário na resposta da API
+            const nomeUsuario = buscarNomeUsuario(data.usuarios, userId);
+
+            // Verificar se o usuário já possui uma avaliação
+            const usuarioAvaliacao = data.avaliacao.find(avaliacao => avaliacao.usuario === nomeUsuario);
+
+            if (usuarioAvaliacao) {
+                // Atualizar a média do usuário existente
+                usuarioAvaliacao.media = calcularMedia(nota, usuarioAvaliacao.media);
+            } else {
+                // Criar uma nova avaliação para o usuário
+                data.avaliacao.push({
+                    usuario: nomeUsuario,
+                    media: nota
+                });
+            }
+
+            // Enviar os dados atualizados para a API
+            fetch('db_usuarios.json', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(response => {
+                    if (response.ok) {
+                        console.log('Média de avaliações atualizada com sucesso!');
+                    } else {
+                        console.error('Erro ao atualizar a média de avaliações.');
+                    }
+                })
+                .catch(error => {
+                    console.error('Erro ao enviar os dados atualizados:', error);
+                });
+        })
+        .catch(error => {
+            console.error('Erro ao obter os dados existentes:', error);
+        });
+}
+
+
+
+// Associar o evento de envio do formulário à função de manipulação
+const form = document.getElementById('avaliacaoForm');
+form.addEventListener('submit', handleFormSubmit);
