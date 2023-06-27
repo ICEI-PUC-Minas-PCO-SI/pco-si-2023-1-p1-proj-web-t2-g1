5948b72ba0d20e63040e0c557b5aaa03e3d48877
anafoc-lab <126624375+anafoc-lab@users.noreply.github.com>
2023-06-26 23:00:23 -0300

    Recuperacao de senha

---
+++ src/recuperarsenha2.js
@@ -0,0 +1,35 @@
+const url = "http://localhost:3000/usuarios";
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOMContentLoaded event triggered.');
+    
+    const form = document.querySelector('form');
+  
+    
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+  
+      
+      const senha = document.getElementById('senha').value;
+      const confirmaSenha = document.getElementById('confirma').value;
+  
+      
+      if (senha !== confirmaSenha) {
+        alert('As senhas não correspondem!');
+        return;
+      }
+  
+    
+      const usuario = {
+        id: 1,
+        senha: senha
+      };
+    
+      axios.put('http://localhost:3000/usuarios/' + usuario.id, usuario)
+        .then(function(response) {
+          console.log('Senha atualizada com sucesso!');
+        })
+        .catch(function(error) {
+          console.error('Erro ao atualizar a senha:', error);
+        });
+    });
+  });
