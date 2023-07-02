b6673f3e83f974451a7104f0ed678dc89f7ca2e5
DerickLRodrigues <dericklucasalves@gmail.com>
2023-07-02 19:51:21 -0300

    Corrigindo Conflito

--- src/TrocarSenha1.js
+++ src/TrocarSenha1.js
@@ -1,25 +1,16 @@
 const URL = 'http://localhost:3000/usuarios';
 
-<<<<<<< HEAD
-function AUTENTICACAO() {
-  const email = document.getElementById('email_input').value;
-  const telefone = document.getElementById('telefone_input').value;
-
-// FUNÇÃO PARA VERIFICAÇÃO DO EMAIL 
-fetch(URL)
-=======
 function realizarVerificacao() {
     const email = document.getElementById('email_input').value;
     const telefone = document.getElementById('telefone_input').value;
 
     fetch(URL)
->>>>>>> fb7e3fe05219e53e1e3c3904b1f0ceed49cad444
         .then(res => res.json())
         .then(usuarios => {
-            const usuarioEncontrado = usuarios.find(usuario => usuario.email === email);
+            const usuarioEncontrado = usuarios.find(usuario => usuario.email === email && usuario.telefone === telefone);
 
             if (usuarioEncontrado) {
-                
+
                 // Usuário válido, redirecionar para a página de sucesso ou executar alguma ação desejada
                 window.location.href = 'TrocarSenha2.html';
             } else {
@@ -32,26 +23,3 @@ function realizarVerificacao() {
             console.error('Erro ao carregar usuários:', error);
         });
 }
-<<<<<<< HEAD
-}
-
-//FUNÇÃO PARA VERIFICAÇÃO DO TELEFONE
-fetch(URL)
-        .then(res => res.json())
-        .then(usuarios => {
-            const usuarioEncontrado = usuarios.find(usuario => usuario.telefone === telefone);
-
-            if (telefoneEncontrado) {
-
-            } else {
-                alert('Telefone inválido. Tente novamente.');
-            }
-        })
-=======
-
-
-
-
-
-
->>>>>>> fb7e3fe05219e53e1e3c3904b1f0ceed49cad444
